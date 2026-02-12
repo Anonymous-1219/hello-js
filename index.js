dee376733074fa4da0e55fcbfda8e96e80716246
Anonymous-1219 <muizope114@gmail.com>
2026-02-12 14:58:12 +0100

    Add personal introduction to console log

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello, world!");
+let name = "Muiz";
+let age = 19;
+let programming_language = "JavaScript";
+
+console.log("My name is " + name + ". I'm " + age + " years old. My favourite programming language is " + programming_language + ".");
